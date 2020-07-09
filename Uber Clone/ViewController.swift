820ece07e0a50c472d5945d4e8aef099d6aa7f5f
Nurlybek Karshiga <nurlybekkarshiga@gmail.com>
2020-07-09 18:51:19 -0400

    Initial Commit

---
+++ Uber Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Uber Clone
+//
+//  Created by Nulrybek Karshyga on 7/9/20.
+//  Copyright © 2020 Nulrybek Karshyga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
